0fdf76f9d4c16703cac19d720832d77dbf0c7329
Guilhem <guilhem.mangenot@gmail.com>
2023-08-10 23:52:00 +0200

    Ajout Guilhem

---
+++ Assets/Scripts/Penguin.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Penguin : MonoBehaviour
+{
+	[Header ("Penguin")]
+	// Stats
+	public float speed;
+	public float slideBoost;
+	public float slideSlowDown;
+	public float fireCooldown;
+	// Prefabs
+	public GameObject projectilePrefab;
+
+	// Déplacements du pingouin
+	protected Vector2 movement = Vector2.zero;
+	protected Vector2 facingDirection = Vector2.down;
+	[HideInInspector] public bool isSliding = false;
+	protected float timeAtLastFire = 0f;
+
+	// Composants
+	protected Animator animator;
+	protected Rigidbody2D body;
+
+	protected void InitPenguin ()
+	{
+		animator = gameObject.GetComponent<Animator> ();
+		body = gameObject.GetComponent<Rigidbody2D> ();
+
+		animator.SetFloat ("speed", 0);
+		animator.SetInteger ("orientation", 0);
+	}
+
+	protected void Move ()
+	{
+		body.velocity = movement;
+	}
+
+	protected IEnumerator slide ()
+	{
+		while (isSliding)
+		{
+			movement.x = slideSlowDown * movement.x;
+			movement.y = slideSlowDown * movement.y;
+			if (Mathf.Abs (movement.x) + Mathf.Abs (movement.y) < 0.1)
+				isSliding = false;
+			yield return new WaitForSeconds (0.5f);
+		}
+	}
+
+	protected void Fire ()
+	{
+		if (isSliding)
+			return;
+		if (Time.time - timeAtLastFire < fireCooldown)
+			return;
+
+		timeAtLastFire = Time.time;
+		GameObject projectile = Instantiate (projectilePrefab, transform.position, Quaternion.identity);
+		projectile.GetComponent<Projectile> ().direction = facingDirection;
+		projectile.GetComponent<Projectile> ().owner = this;
+	}
+}
