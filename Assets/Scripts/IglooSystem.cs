f8730d3f8830cccecae968cdc20c9a0c124d44b1
Voltini <slimen3@hotmail.fr>
2023-08-16 23:07:47 +0200

    Add hint to open shop

--- Assets/Scripts/IglooSystem.cs
+++ Assets/Scripts/IglooSystem.cs
@@ -1,22 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class IglooSystem : MonoBehaviour
 {
-    public GameObject player;
-    private Player playerSystem;
+    [SerializeField]
+    private Canvas shopButton;
 
-    private void Awake() {
-        //Initialisation Component
-        playerSystem = player.GetComponent<Player>();
+    private void Start() {
+        shopButton.gameObject.SetActive(true);
+        shopButton.enabled = false;
     }
-    private void OnCollisionEnter2D(Collision2D collision)
-	{
-		if (collision.gameObject.CompareTag("Shards"))
-		{
-			Destroy(collision.gameObject);
-            playerSystem.iceShards+=1; // Incrementation du score
-		}
-	}
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.CompareTag("Player")) {
+            shopButton.enabled = true;            
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.gameObject.CompareTag("Player")) {
+            shopButton.enabled = false;
+        }
+    }
+    // public GameObject player;
+    // private Player playerSystem;
+
+    // private void Awake() {
+    //     //Initialisation Component
+    //     playerSystem = player.GetComponent<Player>();
+    // }
+    // private void OnCollisionEnter2D(Collision2D collision)
+	// {
+	// 	if (collision.gameObject.CompareTag("Shards"))
+	// 	{
+	// 		Destroy(collision.gameObject);
+    //         playerSystem.iceShards+=1; // Incrementation du score
+	// 	}
+	// }
 }
