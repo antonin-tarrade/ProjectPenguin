7b17c7e235c3aa43f041eb154983fc0582d3fa4b
Remoraki <gaga.gournay@gmail.com>
2023-09-28 09:47:56 +0200

    rework wave system

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -22,6 +22,7 @@ public class EnemySpawner : MonoBehaviour
     public int numberOfEnemies;
     public int numberOfBoss;
     public int numberOfSlime;
+    public List<SpawnData> waveToSpawn;
 
     // Moyenne du cercle d'apparition
     public float spawnRadius;
@@ -41,7 +42,7 @@ public class EnemySpawner : MonoBehaviour
 
 
     public bool modifyStats = false;
-    public Penguin.StatModifier statModifier;
+    //public Penguin.StatModifier statModifier;
 
     private void Awake()
     {
@@ -69,85 +70,45 @@ public class EnemySpawner : MonoBehaviour
         {
             StartCoroutine(SpawnEnemiesWithDelay());
         }
+        */
         if (Input.GetKeyDown(KeyCode.R))
         {
-            foreach (GameObject obj in enemyTrackers) Destroy(obj);
-        }*/
+            ClearWave();
+        }
 
     }
+
 
     IEnumerator SpawnEnemiesWithDelay()
     {
         waves[waveNumber].Load();
-        remainingEnemies = numberOfEnemies + numberOfBoss + numberOfSlime;
-        // Spawn basic ennemies
-        for (int i = 0; i < numberOfEnemies; i++)
+        foreach (SpawnData spawnData in waveToSpawn)
         {
-            // Position 
-            float angle = Random.Range(0f, Mathf.PI * 2);
-            float radius = spawnRadius + Random.Range(-spawnRandom, spawnRandom);
-            Vector3 spawnPosition = transform.position + new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0) * radius;
 
-            // Spawn
-            GameObject enemy = Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
-            enemyTrackers.Add(enemy);
-            Enemy enemyComponent = enemy.GetComponent<Enemy>();
-            enemyComponent.spawner = this;
-            //remainingEnemies++;
-            // Delai
-            // Modifier le minspawndelay, mais le min ne doit pas être trop court pour des problèmes d'opérations effectué sur l'objet instancié
-            float spawnDelay = Random.Range(maxSpawnDelay/2f, maxSpawnDelay);
-            yield return new WaitForSeconds(spawnDelay);
-            enemyComponent.SetStats(GameManager.instance.battleData.enemyStats);
-            if (modifyStats) waves[waveNumber].statModifier.Apply(enemyComponent);
-            enemyComponent.health = enemyComponent.baseHealth;
-        }
+            //remainingEnemies = numberOfEnemies + numberOfBoss + numberOfSlime;
+            remainingEnemies += spawnData.numberOfEnnemies;
+            // Spawn basic ennemies
+            for (int i = 0; i < spawnData.numberOfEnnemies; i++)
+            {
+                // Position 
+                float angle = Random.Range(0f, Mathf.PI * 2);
+                float radius = spawnRadius + Random.Range(-spawnRandom, spawnRandom);
+                Vector3 spawnPosition = transform.position + new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0) * radius;
 
-        // Spawn Bosses
-        for (int i = 0; i < numberOfBoss; i++)
-        {
-            // Position 
-            float angle = Random.Range(0f, Mathf.PI * 2);
-            float radius = spawnRadius + Random.Range(-spawnRandom, spawnRandom);
-            Vector3 spawnPosition = transform.position + new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0) * radius;
-
-            // Spawn
-            GameObject enemy = Instantiate(bossPrefab, spawnPosition, Quaternion.identity);
-            enemyTrackers.Add(enemy);
-            Enemy enemyComponent = enemy.GetComponent<Enemy>();
-            enemyComponent.spawner = this;
-            //remainingEnemies++;
-            // Delai
-            // Modifier le minspawndelay, mais le min ne doit pas être trop court pour des problèmes d'opérations effectué sur l'objet instancié
-            float spawnDelay = Random.Range(maxSpawnDelay/2f, maxSpawnDelay);
-            yield return new WaitForSeconds(spawnDelay);
-            enemyComponent.SetStats(GameManager.instance.battleData.enemyStats);
-            if (modifyStats) waves[waveNumber].statModifier.Apply(enemyComponent);
-            enemyComponent.health = enemyComponent.baseHealth;
-        }
-
-        // Spawn Slimes
-        
-        for (int i = 0; i < numberOfSlime; i++)
-        {
-            // Position 
-            float angle = Random.Range(0f, Mathf.PI * 2);
-            float radius = spawnRadius + Random.Range(-spawnRandom, spawnRandom);
-            Vector3 spawnPosition = transform.position + new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0) * radius;
-
-            // Spawn
-            GameObject enemy = Instantiate(slimePrefab, spawnPosition, Quaternion.identity);
-            enemyTrackers.Add(enemy);
-            Enemy enemyComponent = enemy.GetComponent<Enemy>();
-            enemyComponent.spawner = this;
-            //remainingEnemies++;
-            // Delai
-            // Modifier le minspawndelay, mais le min ne doit pas être trop court pour des problèmes d'opérations effectué sur l'objet instancié
-            float spawnDelay = Random.Range(maxSpawnDelay/2f, maxSpawnDelay);
-            yield return new WaitForSeconds(spawnDelay);
-            enemyComponent.SetStats(GameManager.instance.battleData.enemyStats);
-            if (modifyStats) waves[waveNumber].statModifier.Apply(enemyComponent);
-            enemyComponent.health = enemyComponent.baseHealth;
+                // Spawn
+                GameObject enemy = Instantiate(spawnData.ennemyPrefab, spawnPosition, Quaternion.identity);
+                enemyTrackers.Add(enemy);
+                Enemy enemyComponent = enemy.GetComponent<Enemy>();
+                enemyComponent.spawner = this;
+                //remainingEnemies++;
+                // Delai
+                // Modifier le minspawndelay, mais le min ne doit pas être trop court pour des problèmes d'opérations effectué sur l'objet instancié
+                float spawnDelay = Random.Range(maxSpawnDelay / 2f, maxSpawnDelay);
+                yield return new WaitForSeconds(spawnDelay);
+                enemyComponent.SetStats(GameManager.instance.battleData.enemyStats);
+                if (spawnData.statModifier.modifyStats) spawnData.statModifier.Apply(enemyComponent);
+                enemyComponent.health = enemyComponent.baseHealth;
+            }
         }
     }
 
@@ -189,6 +150,7 @@ public class EnemySpawner : MonoBehaviour
     {
         remainingEnemies = 0;
         foreach (GameObject obj in enemyTrackers) Destroy(obj);
+        WaitForNextWave();
     }
 
     public void StartWave()
---
+++ Assets/Scripts/PenguinStatModifier.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PenguinStatModifier", menuName = "Scriptables/PenguinStatModifier", order = 1)]
+public class PenguinStatModifier : ScriptableObject
+{
+    // Modificateur de stats
+    public bool modifyStats;
+    public bool modifyHealth;
+    public float healthModifier;
+    public bool modifySpeed;
+    public float speedModifier;
+    public bool modifyAttackSpeed;
+    public float attackSpeedModifier;
+    public bool modifyDmg;
+    public float dmgModifier;
+
+    public void Apply(Penguin penguin)
+    {
+        if (modifyHealth) penguin.baseHealth = (int)(penguin.baseHealth * healthModifier);
+        if (modifySpeed) penguin.speed *= speedModifier;
+        if (modifyAttackSpeed) penguin.attack.speed *= attackSpeedModifier;
+        if (modifyDmg) penguin.attack.dmg *= dmgModifier;
+    }
+
+}
+
